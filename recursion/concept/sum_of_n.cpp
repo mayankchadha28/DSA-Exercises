d4fca15cad487d587388057de206302bdd9dcc3c
mayankchadha28 <mayankchadha1998@gmail.com>
2026-03-22 19:51:06 -0400

    factorial recursion

---
+++ recursion/concept/sum_of_n.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+/* 
+
+f(n) = n + f(n-1)
+
+f(4) -> f(3) -> f(2) -> f(1)
+
+Time Complexity = O(n) +O(1)
+Space Complexity = O(n)
+
+Compared to for loop which has same Time Complexity, Space Complexity for for loop is O(1) but for recursion is O(n) due to call stack
+*/
+
+int sum(int n){
+    if(n == 1){
+        return 1;
+    }
+    
+    return n + sum(n-1);
+}
