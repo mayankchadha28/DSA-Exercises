a2a6193feb96fe119869c4b67daa381b5ee6732a
mayankchadha28 <mayankchadha1998@gmail.com>
2026-04-16 17:22:45 -0400

    LinkedList: add number

--- DataStructures/LinkedList/problems/add_two_numbers.cpp
+++ DataStructures/LinkedList/problems/add_two_numbers.cpp
@@ -4,10 +4,73 @@ Leetcode:
 
 */
 
+#include <string>
+
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 
 // Always prefer using a dummy node while creating a new list
+/* 
+This solution creates a new List with a dummy node to track
+
+add the number and store the carry in a variable and add to the sum
+
+then store the 0th index as the node
+
+make sure to check if the linked list is NULL as there may not be equal number of elements
+
+In the last there may be sum carry left out which we need to add so do that and return the output removing the dummy node
 
 
+*/
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode* curr_a = l1;
+        ListNode* curr_b = l2;
+        
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* currNode = dummyNode;
+
+        int carry = 0;
+
+        while(curr_a != NULL || curr_b != NULL){
+            int sum = carry;
+
+            if(curr_a != NULL) sum += curr_a->val;
+            if(curr_b != NULL) sum += curr_b->val;
+            
+            carry = sum / 10;
+            
+            ListNode* newNode = new ListNode(sum % 10);
+            
+            currNode->next = newNode;
+            currNode = currNode->next;
+
+            if(curr_a) curr_a = curr_a->next;
+            if(curr_b) curr_b = curr_b->next;
+
+        }
+
+        if(carry){
+            ListNode* newNode = new ListNode(carry);
+            currNode->next = newNode;
+        }
+
+
+        
+        dummyNode = dummyNode->next;
+        return dummyNode;
+    
+    }
 
 
 
@@ -26,7 +89,7 @@ as to_string returns string but we need to insert char
 */
 
 
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+ListNode* addTwoNumbers_strversion(ListNode* l1, ListNode* l2) {
         string a;
         string b;
         ListNode* currNode = l1;
